82ce1b4148e4d3d3f5dca3f30f87f604ad2d031c
Adam <johnsonam17@gmail.com>
2018-01-24 14:46:22 -0500

    mobile homepage first draft
    mostly setup media queries for responsive homepage; moved some things
    out of javascript that didn't need to be there

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// total number of panels
+var numPanels = $(".panel").length;
+var hoverBreakpoint = 520;
+
+/*
+ * Expand the panel when it is hovered over
+ */
+$(".panel").hover(
+    function(){
+        if($(window).width() > hoverBreakpoint){
+            $(".panel").each(function(){
+                $(this).css("width", 100/(numPanels+1) + "%");
+            });
+            
+            $(this).css("width", (100/(numPanels+1))*2 + "%")
+            .addClass("panel_active");
+        }
+    }, function(){
+        if($(window).width() > hoverBreakpoint){
+            $(".panel").each(function(){
+                $(this).css("width", 100/(numPanels) + "%")
+                .removeClass("panel_active");
+            });
+        }
+    }
+);
